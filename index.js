be6cbafab8283452feba318c96c39c7419a07128
akhil8086 <akhiljith.r@edstem.com>
2023-08-10 14:27:36 +0530

    Get and Post

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-const form = document.Application;
+// const form = document.Application;
+// form.addEventListener("submit", function(event) {
+//   event.preventDefault(); 
+//   const formData = new FormData(this);
+//   const object = {};
+//   formData.forEach((value, key) => {
+//     object[key] = value
+//   });
+//   const json = JSON.stringify(object);
+//   console.log(json);
+//   form.reset();
+// }, false);
+
+
+let userID ;
+let value = document.getElementById("useridHidden");
+document.addEventListener("DOMContentLoaded", function() {
+const form = document.querySelector("form");
+
 form.addEventListener("submit", function(event) {
   event.preventDefault(); 
-  let formData = new FormData(this);
-  let object = {};
+
+  const formData = new FormData(form);
+  const formDataObject = {};
   formData.forEach((value, key) => {
-    object[key] = value
+    formDataObject[key] = value;
   });
-  let json = JSON.stringify(object);
-  console.log(json);
-  form.reset();
-}, false);
+
+  fetch("https://retoolapi.dev/SoBfZA/data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formDataObject)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Response Data:", data);
+        form.reset()
+      userID = data.id
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+});
+});
+viewButton.addEventListener("click", function() {
+  fetch("https://retoolapi.dev/SoBfZA/data")
+    .then(response => response.json())
+    .then(data => {
+      const submittedData = data[userID-1];
+      console.log(typeof(submittedData))
+      value.textContent = JSON.stringify(submittedData);
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+});
